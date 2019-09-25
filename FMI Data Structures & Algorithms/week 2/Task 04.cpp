2aeda537fb0e86ffd93c2e5b0de276924ee570d5
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-09-25 22:20:06 +0300

    Update Task 04.cpp

--- FMI Data Structures & Algorithms/week 2/Task 04.cpp
+++ FMI Data Structures & Algorithms/week 2/Task 04.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 
+size_t countInversions(size_t *arr, size_t n)
+{
+	size_t inversions(0);
+	for (size_t i = 0; i < n - 1; i++)
+	{
+		for (size_t j = i + 1; j < n; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				inversions++;
+			}
+		}
+	}
+	return inversions;
+}
+
 int main()
 {
 	size_t Q; std::cin >> Q;
 	for (size_t i = 0; i < Q; i++)
 	{
-		size_t n, countSoftwareOrder(0);  std::cin >> n;
+		size_t n;  std::cin >> n;
 		size_t *soldiers = new size_t[n];
-		for (size_t i = 0; i < n; i++) std::cin >> soldiers[i];
-
-		for (size_t i = 0; i < n - 1; i++)
-		{
-			for (size_t j = i + 1; j < n; j++)
-			{
-				if (soldiers[i] > soldiers[j])
-				{
-					countSoftwareOrder++;
-				}
-			}
-		}
-		std::cout << countSoftwareOrder << '\n';
+		for (size_t i = 0; i < n; i++) std::cin >> soldiers[i];		
+		std::cout << countInversions(soldiers, n) << '\n';
 		delete[] soldiers;
 	}
 	return 0;
