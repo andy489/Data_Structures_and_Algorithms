ec8448f5b8e5ef7c5924c98c3684dd5bf4ee8fa2
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-09-27 20:09:03 +0300

    Create Task 04.cpp

---
+++ FMI Data Structures & Algorithms/week 4/Task 04.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+struct Snowman
+{
+	unsigned short value = 0;
+	size_t total = 0;
+	Snowman* previuos = nullptr;
+};
+
+int main()
+{
+	size_t N; std::cin >> N;
+	Snowman* arr = new Snowman[N + 1]; //the first one is the zero snowman
+
+	size_t dupTarget = 0;
+	size_t add = 0;
+
+	for (size_t i = 1; i < N + 1; i++)
+	{
+		std::cin >> dupTarget >> add;
+		if (add == 0)
+		{
+			if (arr[dupTarget].previuos)
+			{
+				arr[i] = *arr[dupTarget].previuos;
+			}
+		}
+		else
+		{
+			arr[i].value = add;
+			arr[i].total = arr[dupTarget].total + add;
+			arr[i].previuos = &arr[dupTarget];
+		}
+	}
+
+	size_t sum = 0;
+
+	for (int i = 0; i < N + 1; i++)
+	{
+		sum += arr[i].total;
+	}
+	std::cout << sum;
+
+	return 0;
+}
+
