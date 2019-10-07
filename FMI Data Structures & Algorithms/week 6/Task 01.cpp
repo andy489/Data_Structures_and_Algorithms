764b33dc9142dc4ed07b725fe73c2a17569faa82
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-10-07 21:14:04 +0300

    Update Task 01.cpp

--- FMI Data Structures & Algorithms/week 6/Task 01.cpp
+++ FMI Data Structures & Algorithms/week 6/Task 01.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
 
+struct Node
+{
+	int data;
+	Node* left, * right;
+	Node(int data) :left(nullptr), right(nullptr), data(data) {};
+};
+
+Node* add(Node* root, int data)
+{
+	if (root == nullptr)
+	{
+		root = new Node(data);
+	}
+	else if (data <= root->data)
+	{
+		root->left = add(root->left, data);
+	}
+	else
+	{
+		root->right = add(root->right, data);
+	}
+	return root;
+}
+
+int height(Node* root)
+{
+	if (root == nullptr) return -1;
+	else
+	{
+		int leftHeight = height(root->left);
+		int rightHeight = height(root->right);
+		if (leftHeight > rightHeight) return(leftHeight + 1);
+		else return(rightHeight + 1);
+	}
+}
+
+
+int main()
+{
+	Node* root = nullptr;
+	unsigned countNodes;std::cin >> countNodes;
+	unsigned i(0), data; std::cout << "Enter data for nodes to be inserted: ";
+	for (; i < countNodes; ++i)
+	{
+		std::cin >> data;
+		root = add(root, data);
+	}
+	std::cout << "Height of BST is: " << height(root) << '\n';
+	return 0;
+}
