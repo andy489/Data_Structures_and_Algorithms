171b11432c4b8fe48459aea6078bd989dd6feb28
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-10-14 20:41:42 +0300

    Create Task 14.cpp

---
+++ FMI Data Structures & Algorithms/week 1/Task 14.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+/*
+PROBLEM STATEMENT
+Write a recursive function that checks if
+a given word is a palindrome.
+*/
+
+/*
+Solution
+The idea of the function is as follows:
+If the string has length of one or zero
+then it is a palindrome by definition.
+Otherwise it the string is a palindrome
+when and only when its first and last
+characters are equal and the string between
+them is also a palindrome.
+Time complexity - O(n)
+Space complexity - O(n), where n is
+the length of string we input
+*/
+bool isPalindrome(char* str, int len)
+{
+	if (len <= 1)
+	{
+		return true;
+	}
+	return (str[0] == str[len - 1]) && isPalindrome(str + 1, len - 2);
+}
+
+int main()
+{
+	char str[1000];
+	std::cin >> str;
+	///note that strlen has linear time complexity so it is unwise to call it every time in the function body
+	///that would make the time complexity of the solution quadratic.
+	isPalindrome(str, strlen(str)) ? std::cout << "yes\n" : std::cout << "no\n";
+}
