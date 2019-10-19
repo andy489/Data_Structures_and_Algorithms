610e904585378d08b373a6cc9b2fadcb82d50de1
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-10-19 21:33:37 +0300

    Create Merge Sort Inversions Count.cpp

---
+++ FMI Data Structures & Algorithms/week 2/Merge Sort Inversions Count.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int merge(vector<int>& col, vector<int>& L, vector<int>& R)
+{
+	int leftCount((int)L.size()), rightCount((int)R.size()), i(0), j(0), k(0), inv(0);
+
+	while (i < leftCount && j < rightCount)
+	{
+		if (L[i] < R[j]) col[k++] = L[i++];
+		else
+		{
+			col[k++] = R[j++];
+			inv += leftCount - i;
+		}
+	}
+	while (i < leftCount) col[k++] = L[i++];
+	while (j < rightCount) col[k++] = R[j++];
+	return inv;
+}
+
+int mergeSort(vector<int>& col)
+{
+	int n((int)col.size()), mid, i, inv(0);
+	if (n < 2) return inv;
+
+	mid = n / 2;
+
+	vector<int>L(mid);
+	vector<int>R(n - mid);
+
+	for (i = 0;i < mid;i++) L[i] = col[i];
+	for (i = mid;i < n;i++) R[i - mid] = col[i];
+
+	inv += mergeSort(L);
+	inv += mergeSort(R);
+	inv += merge(col, L, R);
+	return inv;
+}
+
+int main()
+{
+	vector<int> col = { 4,-2,8,9,3,4,-1,0,5,1 };
+	int inv = mergeSort(col);
+	cout << "Count of inversions: " << inv << '\n';
+	for (auto el : col)cout << el << ' ';
+	return 0;
+}
