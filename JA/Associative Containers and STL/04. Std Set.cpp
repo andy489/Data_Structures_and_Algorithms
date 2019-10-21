14370581da0085b9d9250835b1f710b441a67c2f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-10-21 09:40:30 +0300

    Create 04. Std Set.cpp

---
+++ JA/Associative Containers and STL/04. Std Set.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<set>
+#include<string>
+#include<sstream>
+
+int main() 
+{
+    using namespace std;
+
+    set<int> digits { 4, 1, 4, 0, 2, 6, 9, 1, 8, 6, 2, 3, 5, 6, 7 };
+
+    for (int d : digits) 
+	{
+        cout << d << " ";
+    }
+    cout << endl;
+
+    cout << endl;
+
+    set<char> letters;
+    string sentence = "the quick brown fox jumps over the lazy dog";
+    istringstream sentenceIn(sentence);
+    char letter;
+    while (sentenceIn >> letter) 
+	{
+        letters.insert(letter);
+    }
+
+    for (set<char>::iterator i = letters.begin(); i != letters.end(); i++) 
+	{
+        cout << *i;
+    }
+    cout << endl;
+
+    return 0;
+}
