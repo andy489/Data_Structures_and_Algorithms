ebbb175592f6723eb62d03f9dc4d279480f5ff81
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-11-04 21:48:27 +0200

    Create Task 08.cpp

---
+++ FMI Data Structures & Algorithms/week 4/Task 08.cpp
@@ -0,0 +1,21 @@
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    
+    if (position == 0)
+    {
+        SinglyLinkedListNode* temp = head->next;
+        delete head;
+        head = temp;        
+    }
+    else
+    {
+        SinglyLinkedListNode* traverse = head;
+        for (int i = 0; i < position - 1; i++)
+        {
+            traverse = traverse->next;
+        } // traverseNode points to (pos-1)th Node
+        SinglyLinkedListNode* temp = traverse->next; // pos-th Node (node we want to remove)
+        traverse->next = temp->next; // (pos+1)th Node
+        delete temp; // remove temp;        
+    }
+    return head;
+}
