fc42dfbb410f5aa3c1d5a7d414370d1b9e465a18
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-11-05 11:31:53 +0200

    Create Task 09.cpp

---
+++ FMI Data Structures & Algorithms/week 4/Task 09.cpp
@@ -0,0 +1,18 @@
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode* head3(nullptr);
+    
+    if (head1 == nullptr) return(head2);
+    else if (head2 == nullptr) return(head1);
+
+    if (head1->data <= head2->data)
+    {
+        head3 = head1;
+        head3->next = mergeLists(head1->next, head2);
+    }
+    else
+    {
+        head3 = head2;
+        head3->next = mergeLists(head1, head2->next);
+    }
+    return head3;
+}
