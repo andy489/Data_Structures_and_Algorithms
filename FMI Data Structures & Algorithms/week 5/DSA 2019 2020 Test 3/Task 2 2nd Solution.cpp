ff077b7f807e07029aa1bd4f9e8c685e67a40284
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-11-19 22:26:40 +0200

    Create Task 2 2nd Solution.cpp

---
+++ FMI Data Structures & Algorithms/week 5/DSA 2019 2020 Test 3/Task 2 2nd Solution.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    Node *left;
+    Node *right;
+    int value;
+    Node(int value) {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+class BST {
+
+public:
+    BST() {
+        root = NULL;
+    }
+
+    void insert(int value) {
+        root = insert(root, value);
+    }
+
+    void print(Node* root, int k, int currLevel)
+    
+    {
+        if(root==nullptr) return ;
+        print(root->right,k,currLevel+1);
+        if(currLevel==k) cout<<root->value<<' ';
+        print(root->left,k,currLevel+1);
+    }
+    void printKDistant(int k) {
+    print(root,k,0);
+    }
+
+private:
+  	//you can write helper functions if needed
+    Node* root;
+
+    Node* insert(Node *curNode, int value) {
+        if (curNode == NULL) {
+            curNode = new Node(value);
+        } else if (curNode->value < value) {
+            curNode->right = insert(curNode->right, value);
+        } else if (curNode->value > value) {
+            curNode->left = insert(curNode->left, value);
+        } else {
+            //if we already have this value at the tree - do nothing
+        }
+        return curNode;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    int value;
+    BST tree;
+    for(int i = 0 ; i < n; i++) {
+        cin >> value;
+        tree.insert(value);
+    }
+    int k;
+    cin >> k;
+    tree.printKDistant(k);
+    return 0;
+}
