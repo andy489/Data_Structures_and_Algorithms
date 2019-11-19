46f90cff8357a19cbaea2dccd117057a1584082a
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-11-19 10:00:56 +0200

    Create Task 21 Solution.cpp

---
+++ FMI Data Structures & Algorithms/week 5/Task 21 Solution.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getMax(TreeNode* root) {
+        TreeNode* current = root;
+        while (current->right) {
+            current = current->right;
+        }
+       
+        return current->val;
+    }
+   
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) {
+            return root;
+        }
+       
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if (root->left == nullptr) {
+                TreeNode* toReturn = root->right;
+                delete root;
+                return toReturn;
+            } else if (root->right == nullptr) {
+                TreeNode* toReturn = root->left;
+                delete root;
+                return toReturn;
+            } else {
+                int toReplace = getMax(root->left);
+                root->val = toReplace;
+                root->left = deleteNode(root->left, toReplace);
+            }
+        }
+       
+        return root;
+    }
+};
