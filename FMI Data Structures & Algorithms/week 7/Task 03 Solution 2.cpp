ee034c4a95851555dd3d42707080e6c0daaa1fef
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-12-20 22:41:20 +0200

    Create Task 03 Solution 2.cpp

---
+++ FMI Data Structures & Algorithms/week 7/Task 03 Solution 2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+auto inp(int n)
+{
+    vector<string> v;
+    while(n--)
+    {
+        string t;
+        cin >> t;
+        v.push_back(move(t));
+    }
+    return v;
+}
+
+int query(const vector<string>& v, string& s)
+{
+    auto it = lower_bound(v.begin(), v.end(), s);
+    s.back()++;
+    return lower_bound(v.begin(), v.end(), s) - it;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+    
+    auto v = inp(n);
+    sort(v.begin(), v.end());
+    string s;
+    while(q--)
+    {
+        cin >> s;
+        cout << query(v, s) << '\n';
+    }
+}
