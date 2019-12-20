7446814598789e5894874f5616683e99ef17e60f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2019-12-20 22:52:27 +0200

    Update Task 04 Solution.cpp

--- FMI Data Structures & Algorithms/week 7/Task 04 Solution.cpp
+++ FMI Data Structures & Algorithms/week 7/Task 04 Solution.cpp
@@ -1,48 +1,19 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
-int main()
-{
-    int N, i, r, c(0);
-    scanf("%d", &N);
-
-    vector<int> keys, doors;
-    keys.reserve(N);
-    doors.reserve(N);
-
+typedef vector<int> vi;
+#define f(i,n) for(i=0;i<n-1;++i)
+#define s(a) scanf("%d", &a)
+int main() { int n, i, r, c(0); s(n);
+    vi keys(n), doors(n);
     unordered_map<int, int> keyChain;
-
-    for (i = 0;i < N - 1;++i) {
-        scanf("%d", &r);
-        keys.push_back(r);
-    }
-
-    for (i = 0;i < N - 1;++i) {
-        scanf("%d", &r);
-        doors.push_back(r);
-    }
-
-    for (i = 0; i < N - 1; ++i) {
-        keyChain[keys[i]]++;
-        if (keyChain.find(doors[i]) != keyChain.end())
-        {
-            if (keyChain[doors[i]] == 0)
-            {
-                c++;
-            }
-            else
-            {
-                keyChain[doors[i]]--;
-            }
-        }
-        else
-        {
-            c++;
-        }
-    }
-
-    printf("%d", c);
-
+    f(i,n) s(keys[i]);
+    f(i,n) s(doors[i]);
+    f(i,n) { keyChain[keys[i]]++; // collect key
+        if (keyChain.count(doors[i])) {
+            if (keyChain[doors[i]]) keyChain[doors[i]]--;
+            else c++;
+        } else c++;
+    } printf("%d", c);
     return 0;
 }
