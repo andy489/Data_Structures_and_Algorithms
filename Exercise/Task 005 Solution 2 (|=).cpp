206d9a3394a402adf8a36400d15a4afa4c2477b9
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-01-19 23:33:48 +0200

    Update Task 005 Solution 2 (|=).cpp

--- Exercise/Task 005 Solution 2 (|=).cpp
+++ Exercise/Task 005 Solution 2 (|=).cpp
@@ -5,35 +5,25 @@ using namespace std;
 int q, R, C, r, c, i, j,a,b;
 vector<vector<char>> G, g;
 
-void inputGrid(int r, int c, vector<vector<char>>& grid)
-{
-    for (i = 0;i < r;++i)
-    {
-        for (j = 0;j < c;++j)
-        {
+void inputGrid(int r, int c, vector<vector<char>>& grid){
+    for (i = 0;i < r;++i){
+        for (j = 0;j < c;++j){
             cin >> grid[i][j];
         }
     }
 }
 
-bool fingerPrint(int x, int y)
-{
-    for(a=0;a<r;++a)
-    {
-        for(b=0;b<c;++b)
-        {
+bool fingerPrint(int x, int y){
+    for(a=0;a<r;++a){
+        for(b=0;b<c;++b){
             if(g[a][b]!=G[x+a][y+b]) return false;
         }
-    }
-    return true;
+    }return true;
 }
 
-int main()
-{
-    srand((unsigned)time(0));
+int main(){
     cin >> q;L:
-    while (q--)
-    {
+    while (q--){
         bool isPresent(false);
         cin >> R >> C;
         G.assign(1000, vector<char>(1000));
@@ -42,14 +32,11 @@ int main()
         g.assign(1000, vector<char>(1000));
         inputGrid(r, c, g);
 
-        for (i = 0;i < R - r + 1;++i)
-        {
-            for (j = 0;j < C - c + 1;++j)
-            {
+        for (i = 0;i < R - r + 1;++i){
+            for (j = 0;j < C - c + 1;++j){
                 isPresent |= fingerPrint(i, j);                
             }
         }
         cout<<(isPresent?"YES":"NO")<<'\n';        
-    }
-    return 0;
+    }return 0;
 }
