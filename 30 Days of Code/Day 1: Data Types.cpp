8dd5f143665b8cbc61b3a08de1d3b6ba007fcf7a
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-01-23 12:39:10 +0200

    Update Day 1: Data Types.cpp

--- 30 Days of Code/Day 1: Data Types.cpp
+++ 30 Days of Code/Day 1: Data Types.cpp
@@ -8,23 +8,19 @@ int main() {
     int i = 4;
     double d = 4.0;
     string s = "HackerRank ";
-    
     int a;
     double b;
     string c;
-    
-    cin>>a>>b;
-    while(cin>>c)
-    {
+
+    cin >> a;
+    cin >> b;
+    while(cin>>c) {
         s.append(c);
         s.append(" ");
-    }
-    
-    cout<<i+a<<'\n';
-    cout.setf(ios::fixed);
-   
-    cout.precision(1);
-    cout<<d+b<<'\n'<<s;    
-    
+    }   
+
+    cout << (a + i) <<  '\n';
+    cout << setprecision(1) << fixed << (b + d) << '\n';
+    cout << s <<  '\n';
     return 0;
 }
