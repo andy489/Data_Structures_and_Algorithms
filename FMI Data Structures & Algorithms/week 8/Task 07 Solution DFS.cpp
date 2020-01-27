81c15aa6b5ea5b190f568e478588f4bf0cf2a589
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-01-27 02:25:37 +0200

    Update Task 07 Solution DFS.cpp

--- FMI Data Structures & Algorithms/week 8/Task 07 Solution DFS.cpp
+++ FMI Data Structures & Algorithms/week 8/Task 07 Solution DFS.cpp
@@ -4,41 +4,31 @@ using namespace std;
 vector<list<int>> adj;
 vector<bool>visited;
 
-void dfs(int s, bool& cycle) 
-{
-    if (cycle)
-    {
+void dfs(int s, bool& cycle){    
+    if (visited[s]){
+        cycle=true; // back edge
         return;
     }
-    if (visited[s]) 
-    {
-        cycle=true; // back edge
-    }
     visited[s] = true;
-    for (const auto& x : adj[s]) 
-    {
+    for (const auto& x : adj[s]){
         dfs(x, cycle);
     }
     visited[s] = false;        
 }
 
-int main()
-{
+int main(){
     int q, V, E, xi, yi, wi,i;
     cin >> q;
-    while(q--)
-    {   
+    while(q--){   
         bool cycle(false); // cycle detector
         cin >> V >> E;
         adj.assign(V,list<int>());        
         visited.assign(V,0);
-        while(E--)
-        {
+        while(E--){
             cin >> xi >> yi >> wi;--xi;--yi;
             adj[xi].push_back(yi);
         }
-        for (i = 0; i < V; ++i)
-        {
+        for (i = 0; i < V; ++i){
             dfs(i, cycle);            
         }
         cout <<(cycle?"true ":"false ");        
