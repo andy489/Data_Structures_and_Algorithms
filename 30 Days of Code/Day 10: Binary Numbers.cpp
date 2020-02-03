b32e6c51e63190910079cb79d24b19fca4708de8
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-03 20:36:50 +0200

    Create Day 10: Binary Numbers.cpp

---
+++ 30 Days of Code/Day 10: Binary Numbers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int consecutive_ones(int n){
+    int max_count(0);
+    int temp_count(0);
+    while(n){
+        if(n%2){
+            ++temp_count;
+            if(temp_count>max_count){
+                max_count=temp_count;
+            }
+        }
+        else{
+            temp_count=0;            
+        }
+        n/=2;        
+    }
+    return max_count;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout<<consecutive_ones(n);
+    return 0;
+}
