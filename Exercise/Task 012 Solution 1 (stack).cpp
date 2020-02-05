ea9a33553c4fe05b03bd819c279b18f61220006e
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-06 00:18:45 +0200

    Update Task 012 Solution 1 (stack).cpp

--- Exercise/Task 012 Solution 1 (stack).cpp
+++ Exercise/Task 012 Solution 1 (stack).cpp
@@ -3,8 +3,7 @@
 #include <stack>
 using namespace std;
 
-int main()
-{
+int main(){
     stack<int>S;
     stack<char>R;
 
@@ -12,54 +11,43 @@ int main()
 
     int q, type, k;
     cin >> q;
-    while (q--)
-    {
+    while (q--){
         cin >> type;
-
-        switch (type)
-        {
+        switch (type){
         case 1:
             cin >> txt;
             s.append(txt);
             S.push(txt.length());
             S.push(1);
             break;
-
         case 2:
             int i;
             cin >> k;
-            for (i = 0;i < k;++i)
-            {
+            for (i = 0;i < k;++i){
                 R.push(s.back());
                 s.pop_back();
             }
             S.push(k);
             S.push(2);
             break;
-
         case 3:
             cin >> k;
             cout << s[k-1] << '\n';
             break;
-
         case 4:
             type = S.top();
             S.pop();
-            if (type == 1)
-            {
+            if (type == 1){
                 int length = S.top();
                 S.pop();
-                for (i = 0;i < length;++i)
-                {
+                for (i = 0;i < length;++i){
                     s.pop_back();
                 }
             }
-            else
-            {
+            else{
                 int size = S.top();
                 S.pop();
-                for (i = 0;i < size;++i)
-                {
+                for (i = 0;i < size;++i){
                     s.push_back(R.top());
                     R.pop();
                 }
