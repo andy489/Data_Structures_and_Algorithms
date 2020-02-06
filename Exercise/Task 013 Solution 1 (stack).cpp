9e6ca921a96c2d6f219a80b96d8f85c1a0cba5ee
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-06 14:48:59 +0200

    Create Task 013 Solution 1 (stack).cpp

---
+++ Exercise/Task 013 Solution 1 (stack).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n,i;
+    cin >> n;
+
+    vector<long long>arr(n),left(n),right(n);
+    stack<int>s;
+
+    long long tot_max(0);
+
+    for (i = 0;i < n;++i){
+        cin >> arr[i];
+        while (!s.empty() && arr[s.top()] <= arr[i]){
+            s.pop();
+        }
+        if (!s.empty()){
+            left[i] = s.top() + 1;
+        }
+        s.push(i);
+    }
+
+    while (!s.empty()) {
+        s.pop();
+    }
+
+    for (i = n - 1;i >= 0;--i){
+        while (!s.empty() && arr[s.top()] <= arr[i]){
+            s.pop();
+        }
+        if (!s.empty()){
+            right[i] = s.top()+1;
+            tot_max = max(tot_max, left[i] * right[i]);
+        }
+        s.push(i);
+    }
+
+    cout << tot_max;
+
+    return 0;
+}
