e2afe0031c6c0e2882f1b452c8f0f5c29a6ad2ac
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-07 17:57:50 +0200

    Create Task 018 Solution 2 (mapping).cpp

---
+++ Exercise/Task 018 Solution 2 (mapping).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map<string, int>prefixes;
+    string name, oper;
+    cin.tie(nullptr);
+    int q;
+    cin >> q;
+    while (q--){
+        cin >> oper >> name;
+        if (oper == "add"){
+            string prefix("");
+            for (const char& c : name){
+                prefix+=c;
+                prefixes[prefix]++;
+            }
+        }
+        else{
+            cout << prefixes[name] << '\n';
+        }
+    }       
+    return 0;
+}
