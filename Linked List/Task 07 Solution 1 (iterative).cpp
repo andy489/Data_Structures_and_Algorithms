a2f8f7aa6ef391ebef0fcda049972621a26ff0a2
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-09 00:14:10 +0200

    Update Task 07 Solution 1 (iterative).cpp

--- Linked List/Task 07 Solution 1 (iterative).cpp
+++ Linked List/Task 07 Solution 1 (iterative).cpp
@@ -1,1 +1,17 @@
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data){
+    DoublyLinkedListNode* insertNode=new DoublyLinkedListNode(data);
+    if(!head) return insertNode;
+    else if(head->data>data){
+        DoublyLinkedListNode* temp=head;
+        head=insertNode;
+        insertNode->next=temp;
+        temp->prev=insertNode;
+    }
+    else{
+        DoublyLinkedListNode* traverseNode=head;
+        while(traverseNode!=nullptr&&traverseNode->data<data){
+            
+        }
+    }
+}
 
