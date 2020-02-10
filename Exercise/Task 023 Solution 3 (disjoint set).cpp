d5c7688155a5624ba113ddfa62de21f5f42dd100
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-11 01:55:04 +0200

    Create Task 023 Solution 3 (disjoint set).cpp

---
+++ Exercise/Task 023 Solution 3 (disjoint set).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#define MAXN (int)10e5
+int p[MAXN], cnt[MAXN];
+int parent(int x){ 
+    return p[x] = p[x] == x ? x : parent(p[x]); 
+}
+int main(){
+    int n,m,i,x,y;
+    cin>>n>>m;
+    for(i=0;i<n;++i){
+        p[i]=i;        
+    }
+    for(i=0;i<m;++i){        
+        cin >> x >> y;
+        int px = parent(x);
+        int py = parent(y);
+        p[px] = py;
+    }
+    for(i=0;i<n;++i){        
+        ++cnt[parent(i)];
+    }
+    long long sum = n;    
+    sum *= sum;
+    for(const int& x:cnt)
+        sum -= x*x;
+    cout << sum/2;
+    return 0;
+}
