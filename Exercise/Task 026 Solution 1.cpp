6521a3f84fa00b94e4441b8ceca3e1d8f3d5af3f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-12 01:15:18 +0200

    Create Task 026 Solution 1.cpp

---
+++ Exercise/Task 026 Solution 1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,x;
+    int best_sum(0), cur_sum(0);
+    cin>>n;
+    while(n--){
+        cin>>x;
+        x=x==0?-INT_MIN:x;
+        cur_sum=max(0,cur_sum+x);
+        best_sum=max(cur_sum,best_sum);       
+    }
+    cout<<best_sum;
+    return 0;
+}
