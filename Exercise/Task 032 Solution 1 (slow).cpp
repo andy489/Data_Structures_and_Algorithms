d798d79366ffa9b7cc499ca73390bf92b212cff6
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-15 17:09:57 +0200

    Create Task 032 Solution 1 (slow).cpp

---
+++ Exercise/Task 032 Solution 1 (slow).cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1509;
+int n;
+int a[N];
+
+void input() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+}
+
+void sol() {
+    int K = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            K ^= (a[i] > a[j]);
+        }
+    }
+    if (K) {
+        printf("YES\n");
+    }
+    else {
+        printf("NO\n");
+    }
+}
+
+int main() {
+    int test;
+    scanf("%d", &test);
+    while (test--){
+        input();
+        sol();
+    }
+    return 0;
+}
