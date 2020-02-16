7abca04fc3367fd0dac6c40f205a77ce1260582f
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-16 12:17:08 +0200

    Create Day 25: Running Time and Complexity.cpp

---
+++ 30 Days of Code/Day 25: Running Time and Complexity.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool is_prime(size_t n){
+    size_t i;
+    if(n==1) {
+        return false;
+    }
+    for(i=2;i*i<=n;++i){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    size_t n,p;
+    cin>>n;
+    while(n--){
+        cin>>p;
+        cout<<(is_prime(p)?"Prime\n":"Not prime\n");
+    }
+    return 0;
+}
