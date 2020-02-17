e0450c27ab1fb6e2d0eded4ec137b35b816b60f1
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-02-17 22:27:38 +0200

    Update Day 09: Solution.cpp

--- 30 Days of Code/Day 09: Solution.cpp
+++ 30 Days of Code/Day 09: Solution.cpp
@@ -7,19 +7,13 @@ int factorial(int n) {
     else return n*factorial(n-1);
 }
 
-int main()
-{
+int main(){
     ofstream fout(getenv("OUTPUT_PATH"));
-
     int n;
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
     int result = factorial(n);
-
     fout << result << "\n";
-
     fout.close();
-
     return 0;
 }
