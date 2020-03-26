e8b4804deed4d95ee29d5897e59ff43f703a85ae
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 22:36:38 +0200

    Create Remove Duplicates.cpp

---
+++ C++/Linked List/Remove Duplicates.cpp
@@ -0,0 +1,89 @@
+// github.com/andy489
+
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+         
+          Node* removeDuplicates(Node *head)
+          {
+            //Write your code here
+            Node* traverse=head;
+            while(traverse&&traverse->next){
+                if(traverse->data==traverse->next->data){
+                    Node* duplicate=traverse->next;
+                    traverse->next=duplicate->next;
+                    delete duplicate;
+                }
+                else{
+                    traverse=traverse->next;
+                }
+            }
+            return head;
+          }
+
+          Node* insert(Node *head,int data)
+          {
+               Node* p=new Node(data);
+               if(head==NULL){
+                   head=p;  
+
+               }
+               else if(head->next==NULL){
+                   head->next=p;
+
+               }
+               else{
+                   Node *start=head;
+                   while(start->next!=NULL){
+                       start=start->next;
+                   }
+                   start->next=p;   
+
+               }
+                    return head;
+                
+            
+          }
+          void display(Node *head)
+          {
+                  Node *start=head;
+                    while(start)
+                    {
+                        cout<<start->data<<" ";
+                        start=start->next;
+                    }
+           }
+};
+			
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+    head=mylist.removeDuplicates(head);
+
+	mylist.display(head);
+		
+}
