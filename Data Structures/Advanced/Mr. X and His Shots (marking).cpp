a542ba46713620b023a382c6ceeb7fb537c014e7
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 17:27:07 +0200

    Create Mr. X and His Shots (marking).cpp

---
+++ Data Structures/Advanced/Mr. X and His Shots (marking).cpp
@@ -0,0 +1,29 @@
+// github.com/andy489
+
+#include <iostream>
+using namespace std;
+#define FIELD (size_t)15e6
+
+int main() {
+    int N, M, a, b, i, c, d;
+    cin >> N >> M;
+
+    int* opening = new int[FIELD];
+    int* closing = new int[FIELD];
+
+    while (N--) {
+        cin >> a >> b;
+        opening[a] += 1;
+        closing[++b] += 1;
+    }
+    for (i = 0;i < FIELD - 1;++i) {
+        opening[i + 1] += opening[i];
+        closing[i + 1] += closing[i];
+    }
+    int overlapping(0);
+    while (M--) {
+        cin >> c >> d;
+        overlapping+=opening[d]-closing[c];
+    }
+    cout << overlapping;
+}
