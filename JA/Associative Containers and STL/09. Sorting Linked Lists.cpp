77bbf8b2ac0ea4b5ffc90b47925094aa81af894c
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 23:40:08 +0200

    Update 09. Sorting Linked Lists.cpp

--- JA/Associative Containers and STL/09. Sorting Linked Lists.cpp
+++ JA/Associative Containers and STL/09. Sorting Linked Lists.cpp
@@ -1,23 +1,22 @@
+// github.com/andy489
+
 #include<algorithm>
 #include<iostream>
 #include<list>
 
-int main() 
-{
+int main() {
     using namespace std;
 
     list<int> nums { 61, 41, 231, 764, 45 };
     nums.sort();
 
-    for (int num : nums) 
-    {
+    for (int num : nums) {
         cout << num << " ";
     }
     cout << endl;
 
     nums.sort(std::greater<int>());
-    for (int num : nums) 
-    {
+    for (int num : nums) {
         cout << num << " ";
     }
     cout << endl;
