0a5cc3d36c3227b84a8c91ffca4509b3d5cd5369
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 23:36:52 +0200

    Update 05. Unordered map.cpp

--- JA/Associative Containers and STL/05. Unordered map.cpp
+++ JA/Associative Containers and STL/05. Unordered map.cpp
@@ -1,14 +1,14 @@
+// github.com/andy489
+
 #include<iostream>
 #include<string>
 #include<utility>
 #include<unordered_map>
 
-int main() 
-{
+int main() {
     using namespace std;
 
-    unordered_map<string, int> cityPopulations = 
-    {
+    unordered_map<string, int> cityPopulations = {
         pair<string, int>{"Gabrovo", 58950},
         pair<string, int>{"Sofia", 58950},
         pair<string, int>{"Melnik", 385},
@@ -18,8 +18,7 @@ int main()
     cityPopulations["Veliko Tarnovo"] = 72938;
     cityPopulations.insert(pair<string, int>("Pliska", 0));
 
-    for (unordered_map<string, int>::iterator i = cityPopulations.begin(); i != cityPopulations.end(); i++) 
-    {
+    for (unordered_map<string, int>::iterator i = cityPopulations.begin(); i != cityPopulations.end(); i++) {
         cout << i->first << " " << i->second << endl;
     }
 
@@ -28,8 +27,7 @@ int main()
     // "Pliska" isn't really a city, it's an ancient settlement, let's remove it
     cityPopulations.erase("Pliska");
 
-    for (pair<string, int> element : cityPopulations) 
-    {
+    for (pair<string, int> element : cityPopulations) {
         cout << element.first << " " << element.second << endl;
     }
 
@@ -40,11 +38,9 @@ int main()
     getline(cin, searchCityName);
 
     unordered_map<string, int>::iterator foundCity = cityPopulations.find(searchCityName);
-    if (foundCity != cityPopulations.end()) 
-    {
+    if (foundCity != cityPopulations.end()) {
         cout << foundCity->first << " " << foundCity->second << endl;
-    } else 
-    {
+    } else {
         cout << "No information about " << searchCityName << endl;
     }
 
