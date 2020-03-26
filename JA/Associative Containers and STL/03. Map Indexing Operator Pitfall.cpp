a128772adc31601d4a17ab9e3bb8fa23a1a5f27e
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 23:34:27 +0200

    Update 03. Map Indexing Operator Pitfall.cpp

--- JA/Associative Containers and STL/03. Map Indexing Operator Pitfall.cpp
+++ JA/Associative Containers and STL/03. Map Indexing Operator Pitfall.cpp
@@ -1,13 +1,13 @@
+// github.com/andy489
+
 #include<iostream>
 #include<map>
 using namespace std;
 
-int main() 
-{
+int main() {
     map<int, string> numberNames { {2, "two"} };
 
-    for (int i = 0; i < numberNames.size(); i++) 
-	{
+    for (int i = 0; i < numberNames.size(); i++) {
         cout << numberNames[i] << ",";
     }
 
