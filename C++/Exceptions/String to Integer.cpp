c7148a04f947a478f9c73fb793484c87baf748f8
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 22:23:42 +0200

    Create String to Integer.cpp

---
+++ C++/Exceptions/String to Integer.cpp
@@ -0,0 +1,19 @@
+// github.com/andy489
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    try{
+        int n;
+        n=stoi(s);
+        cout<<n;
+    }
+    catch(...){
+        cout<<"Bad String";
+    }
+    return 0;
+}
