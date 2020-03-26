3e3c48436272cc66dc2c7c2a51e0ebd3530faa34
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 23:39:38 +0200

    Update 10. Searching Algorithms.cpp

--- JA/Associative Containers and STL/10. Searching Algorithms.cpp
+++ JA/Associative Containers and STL/10. Searching Algorithms.cpp
@@ -1,20 +1,19 @@
+// github.com/andy489
+
 #include<algorithm>
 #include<iostream>
 #include<vector>
 
-int main() 
-{
+int main() {
     using namespace std;
 
     vector<int> nums { 61, 41, 231, 764, 45 };
     auto it = find(nums.begin(), nums.end(), 231);
 
-    if (it != nums.end()) 
-    {
+    if (it != nums.end()) {
         cout << "found " << *it << " at " << it - nums.begin() << endl;
     } 
-    else 
-    {
+    else {
         cout << "not found" << endl;
     }
 
