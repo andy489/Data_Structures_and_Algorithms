5c7eb14e079e1c89f00c8b74936ca90f9be1e652
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 22:20:30 +0200

    Create Insert Function.cpp

---
+++ C++/Linked List/Insert Function.cpp
@@ -0,0 +1,51 @@
+// github.com/andy489
+
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method          
+          if(!head){
+            return new Node(data);
+          }
+          else{
+            head->next=insert(head->next,data);
+          }
+          return head;
+      }
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
