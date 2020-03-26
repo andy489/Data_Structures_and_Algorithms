6758286914251218cdd9ad9e3661bf16b061236b
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-27 00:46:10 +0200

    Update Full Wave Method BFS.cpp

--- Full Wave Method BFS.cpp
+++ Full Wave Method BFS.cpp
@@ -1,3 +1,5 @@
+// github.com/andy489
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -5,8 +7,7 @@
 #include <sstream>
 using namespace std;
 
-struct Cell
-{
+struct Cell {
 	int x, y, level;
 	bool visited, accessible;
 	Cell* prev = nullptr;
@@ -14,27 +15,21 @@ struct Cell
 		visited(visited), accessible(accessible), level(level), x(x), y(y) {};
 };
 
-struct Maze
-{
+struct Maze {
 	vector<int> row{ 0,1,0,-1 }, col{ -1,0,1,0 };
 	int w, h;
 	vector<vector<Cell>> maze;
-	Maze(int w, int h) :w(w), h(h)
-	{
+	Maze(int w, int h) :w(w), h(h) {
 		maze = vector<vector<Cell>>(h);
-		for (int i = 0; i < w; i++)
-		{
+		for (int i = 0; i < w; i++) {
 			maze[i] = vector<Cell>(h);
 		}
 	}
 
-	void readInput()
-	{
+	void readInput() {
 		int i, j;
-		for (i = 0; i < h; ++i)
-		{
-			for (j = 0; j < w; ++j)
-			{
+		for (i = 0; i < h; ++i) {
+			for (j = 0; j < w; ++j) {
 				cin >> maze[i][j].accessible;
 				maze[i][j].x = i;
 				maze[i][j].y = j;
@@ -42,14 +37,12 @@ struct Maze
 		}
 	}
 
-	bool valid(int x, int y)
-	{
+	bool valid(int x, int y) {
 		return x >= 0 && y >= 0 && x < h && y < w
 			&& maze[x][y].accessible == 0 && maze[x][y].visited == 0;
 	}
 
-	void computePaths(int startX, int startY)
-	{
+	void computePaths(int startX, int startY) {
 		queue<Cell*>Q;
 		maze[startX][startY].visited = 1;
 		maze[startX][startY].level = 0;
@@ -59,17 +52,14 @@ struct Maze
 
 		Q.push(dummy);
 		int X, Y;
-		while (!Q.empty())
-		{
+		while (!Q.empty()) {
 			currCell = Q.front();
 			Q.pop();
 			size_t SIZE = row.size();
-			for (size_t i = 0; i < SIZE; ++i)
-			{
+			for (size_t i = 0; i < SIZE; ++i) {
 				X = currCell->x + row[i];
 				Y = currCell->y + col[i];
-				if (valid(X, Y))
-				{
+				if (valid(X, Y)) {
 					maze[X][Y].level = currCell->level + 1;
 					maze[X][Y].visited = 1;
 					maze[X][Y].prev = currCell;
@@ -78,29 +68,24 @@ struct Maze
 			}
 		}
 	}
-	int getLevel(int x, int y)
-	{
+	int getLevel(int x, int y) {
 		return maze[x][y].level;
 	}
 
-	ostringstream getPath(int x, int y)
-	{
+	ostringstream getPath(int x, int y) {
 		stack<Cell*> S;
 		Cell* end = &maze[x][y];
-		while (end != nullptr)
-		{
+		while (end != nullptr) {
 			S.push(end);
 			end = end->prev;
 		}
 		ostringstream ostr;
-		if (S.empty())
-		{
+		if (S.empty()) {
 			ostr << "no path\n";
 			return ostr;
 		}		
 		ostr << "path:\n";
-		while (!S.empty())
-		{
+		while (!S.empty()) {
 			ostr << "[" << S.top()->x << "," << S.top()->y << "]\n";
 			S.pop();
 		}
@@ -108,13 +93,10 @@ struct Maze
 		return ostr;
 	}
 
-	void displayMazeLevels()
-	{
+	void displayMazeLevels() {
 		int i, j;
-		for (i = 0; i < h; ++i)
-		{
-			for (j = 0; j < w; ++j)
-			{
+		for (i = 0; i < h; ++i) {
+			for (j = 0; j < w; ++j) {
 				cout << maze[i][j].level << "  ";
 			}
 			cout << '\n';
@@ -123,8 +105,7 @@ struct Maze
 };
 
 
-int main()
-{
+int main() {
 	int M, N, K, i, startX, startY, endX, endY;
 	cin >> M >> N >> K >> startX >> startY;
 	Maze maze(M, N);
@@ -136,22 +117,18 @@ int main()
 	queue<int>results;
 	queue<ostringstream>paths;
 
-	for (i = 0; i < K; ++i)
-	{
+	for (i = 0; i < K; ++i) {
 		cin >> endX >> endY;
-		if (endX >= 0 && endY >= 0 && endX < M && endY < N && maze.maze[endX][endY].level!=-1)
-		{
+		if (endX >= 0 && endY >= 0 && endX < M && endY < N && maze.maze[endX][endY].level!=-1) {
 			results.push(maze.getLevel(endX, endY));
 			paths.push(maze.getPath(endX,endY));
 		}
-		else
-		{
+		else {
 			results.push(-1);		
 			paths.push(ostringstream("no path\n-----\n"));
 		}		
 	}
-	while (!results.empty())
-	{
+	while (!results.empty()) {
 		cout << "distance "<<results.front() << '\n';
 		results.pop();
 		cout<<paths.front().str();
