eb82024bfa4869708f547155c1e1124e41fac12d
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-03-26 22:05:25 +0200

    Create Phone Book.cpp

---
+++ C++/STL/Phone Book.cpp
@@ -0,0 +1,26 @@
+// github.com/andy489
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int q,i;
+    cin>>q;
+    string name,phone;
+    unordered_map<string, string>phoneBook;
+    while(q--){
+        cin>>name>>phone;
+        phoneBook[name]=phone;
+    }
+    while(cin>>name){        
+        if(phoneBook.count(name)){
+            cout<<name<<'='<<phoneBook[name]<<'\n';
+        }
+        else{
+            cout<<"Not found\n";
+        }
+    }
+    return 0;
+}
