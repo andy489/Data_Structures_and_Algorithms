f4ae45e17a917c11b7dbceb23ab998ff8607edd9
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-09 21:37:15 +0300

    Create 03.06. Strawberries S1.cpp

---
+++ FMI Data Structures & Algorithms/week 03/03.06. Strawberries S1.cpp
@@ -0,0 +1,47 @@
+// github.com/andy489
+
+// https://www.hackerrank.com/contests/practice-3-sda/challenges/strawberries-sda
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+#define F(i, k, n) for(ll i=k;i<n;i++)
+
+int upper_bound(const vll &vec, ll l, ll r, ll target) {
+    while (l < r) {
+        ll mid = (l + r) / 2;
+        if (target > vec[mid])
+            l = mid + 1;
+        else
+            r = mid;
+    }
+    if (vec[l] >= target)
+        return l;
+    else
+        return -1;
+}
+
+int main() {
+    ll n;
+    cin >> n;
+    vll strawberries(n);
+    F(i, 0, n) cin >> strawberries[i];
+    int m, k;
+    cin >> m;
+    vll tasty(m);
+    F(i, 0, m) cin >> tasty[i];
+
+    vll utility(n);
+    utility[0] = strawberries[0];
+    F(i, 1, n) utility[i] = strawberries[i] + utility[i - 1];
+
+    F(i, 0, m) {
+        ll currStrawberry = tasty[i];
+        ll indx = upper_bound(utility, 0, n - 1, tasty[i]);
+        cout << indx + 1 << '\n';
+    }
+    return 0;
+}
