53b16890b87640ef798fa7c2d4cba7838ec16f6c
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-10 21:12:46 +0300

    Create 05.01. Josephus problem S3 (calculus).cpp

---
+++ FMI Data Structures & Algorithms/week 05/05.01. Josephus problem S3 (calculus).cpp
@@ -0,0 +1,12 @@
+// github.com/andy489
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long n;
+    cin >> n;
+    return cout << 1l + 2l * (n - (long)pow(2, (int) log2(n))), 0;
+}
