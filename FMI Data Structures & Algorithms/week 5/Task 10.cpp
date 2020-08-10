6ac5ffc7f320529a5e12c3f07ac61882d1264073
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 00:22:08 +0300

    Delete Task 10.cpp

--- FMI Data Structures & Algorithms/week 5/Task 10.cpp
+++
@@ -1,6 +0,0 @@
-Node* insert(Node* root, int data) {
-	if (root == nullptr) return new Node(data);
-	else if (root->data > data) root->left = insert(root->left, data);
-	else root->right = insert(root->right, data);
-	return root;
-}
