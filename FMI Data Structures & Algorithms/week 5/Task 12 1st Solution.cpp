8560194a28532441cc31efff1e14ffd42803a790
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 00:43:06 +0300

    Delete Task 12 1st Solution.cpp

--- FMI Data Structures & Algorithms/week 5/Task 12 1st Solution.cpp
+++
@@ -1,7 +0,0 @@
-Node *lca(Node *root, int v1,int v2) 
-{
-    if(root==nullptr) return nullptr;
-    if (root->data<v1 && root->data< v2) return lca(root->right,v1,v2);
-    if (root->data>v1 && root->data > v2) return  lca(root->left,v1,v2);
-    return root;
-}
