1eb8182488e50e0bb6b220f0805bed3f7d08e30e
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 14:24:26 +0300

    Create 06.13. Dundee the crocodile S3.cpp

---
+++ FMI Data Structures & Algorithms/week 06/06.13. Dundee the crocodile S3.cpp
@@ -0,0 +1,23 @@
+// github.com/andy489
+
+// https://www.hackerrank.com/contests/sda-exam-27-01-19-/challenges/dundee-the-crocodile/problem
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    map<string, size_t> m;
+
+    string input;
+
+    while (cin >> input)
+        ++m[input];
+
+    for (const auto &kvp : m)
+        if (kvp.second == 1)
+            cout << kvp.first << '\n';
+
+    return 0;
+}
