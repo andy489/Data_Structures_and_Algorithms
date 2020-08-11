454d53b75e8d7cc75eada3602041e6122cb37939
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 10:13:12 +0300

    Create 05.20. K-th smallest element in a BST S1.cpp

---
+++ FMI Data Structures & Algorithms/week 05/05.20. K-th smallest element in a BST S1.cpp
@@ -0,0 +1,32 @@
+// github.com/andy489
+
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode *root, vector<int> &traversal) {
+        if (root == nullptr) {
+            return;
+        }
+
+        inorder(root->left, traversal);
+        traversal.push_back(root->val);
+        inorder(root->right, traversal);
+    }
+
+    int kthSmallest(TreeNode *root, int k) {
+        vector<int> traversal;
+        inorder(root, traversal);
+
+        return traversal[k - 1];
+    }
+};
