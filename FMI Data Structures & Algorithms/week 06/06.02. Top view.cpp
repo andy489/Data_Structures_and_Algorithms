3b44b2085a447ea959453cb00046e708ec5a3d67
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 11:37:39 +0300

    Create 06.02. Top view.cpp

---
+++ FMI Data Structures & Algorithms/week 06/06.02. Top view.cpp
@@ -0,0 +1,26 @@
+// github.com/andy489
+
+// https://www.hackerrank.com/challenges/tree-top-view
+
+void topView(Node *root) {
+    if (root == nullptr)
+        return;
+    queue <pair<int, Node *>> q;
+    int hd(0);
+    q.push({hd, root});
+
+    map<int, int> m;
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        hd = cur.first;
+        if (cur.second->left)
+            q.push({hd - 1, cur.second->left});
+        if (cur.second->right)
+            q.push({hd + 1, cur.second->right});
+        if (!m.count(hd))
+            m[hd] = cur.second->data;
+    }
+    for (const auto &kvp:m)
+        cout << kvp.second << ' ';
+}
