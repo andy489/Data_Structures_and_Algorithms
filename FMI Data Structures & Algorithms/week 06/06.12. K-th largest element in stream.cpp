0ab03d79f23833e552b35ded6c1517212a3dc1eb
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 14:15:04 +0300

    Create 06.12. K-th largest element in stream.cpp

---
+++ FMI Data Structures & Algorithms/week 06/06.12. K-th largest element in stream.cpp
@@ -0,0 +1,41 @@
+// github.com/andy489
+
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest {
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int k;
+public:
+    KthLargest(int k, vector<int> &nums) : k(k) {
+        int size(nums.size()), i(0);
+
+        for (; i < size; ++i) {
+            if (pq.size() < k)
+                minHeap.push(nums[i]);
+            else {
+                if (nums[i] > pq.top()) {
+                    minHeap.pop();
+                    minHeap.push(nums[i]);
+                }
+            }
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k)
+            minHeap.push(val);
+        else {
+            if (val > pq.top()) {
+                minHeap.pop();
+                minHeap.push(val);
+            }
+        }
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
