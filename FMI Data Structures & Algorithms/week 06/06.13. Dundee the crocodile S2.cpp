f015b084ff0134cdf13e8efdcce0dadd65946de3
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-11 14:22:54 +0300

    Create 06.13. Dundee the crocodile S2.cpp

---
+++ FMI Data Structures & Algorithms/week 06/06.13. Dundee the crocodile S2.cpp
@@ -0,0 +1,30 @@
+// github.com/andy489
+
+// https://www.hackerrank.com/contests/sda-exam-27-01-19-/challenges/dundee-the-crocodile/problem
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    multiset<string> ms;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string input;
+
+    while (cin >> input)
+        ms.insert(input);
+
+    auto it = ms.begin();
+    while (it != ms.end()) {
+        int count = ms.count(*it);
+        if (count == 1)
+            cout << *it << endl;
+        it++;
+    }
+
+    return 0;
+}
