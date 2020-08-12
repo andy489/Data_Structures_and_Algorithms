aff00ccba18b941c6e823f4c66853340058eb63b
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-12 09:30:08 +0300

    Create 08.03. Breadth First Search - shortest reach S2.cpp

---
+++ FMI Data Structures & Algorithms/week 08/08.03. Breadth First Search - shortest reach S2.cpp
@@ -0,0 +1,67 @@
+// github.com/andy489
+
+// https://www.hackerrank.com/challenges/bfsshortreach/problem
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <deque>
+
+using namespace std;
+
+auto bfs(int s, const vector<unordered_set<int>> &adj, int level = 6) {
+    int N(adj.size()), curr, SIZE(1);
+    unordered_set<int> visited;
+    vector<int> ans(N, -1);
+
+    deque<int> Q;
+    Q.push_back(s);
+    visited.insert(s);
+
+    while (!Q.empty()) {
+        curr = Q.front();
+        Q.pop_front();
+        SIZE--;
+
+        for (const auto &x : adj[curr]) {
+            if (!visited.count(x)) {
+                ans[x] = level;
+                Q.push_back(x);
+                visited.insert(x);
+            }
+        }
+        if (SIZE == 0) {
+            SIZE = Q.size();
+            level += 6;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int q, i, j, n, m, a, b, s, k;
+    cin >> q;
+
+    while (q--) {
+        cin >> n >> m;
+
+        vector<unordered_set<int>> adj(n + 1);
+
+        for (j = 0; j < m; ++j) {
+            cin >> a >> b;
+            adj[a].insert(b);
+            adj[b].insert(a);
+        }
+
+        cin >> s;
+        vector<int> ans = bfs(s, adj);
+
+        for (k = 1; k <= n; ++k) {
+            if (k != s) {
+                cout << ans[k] << ' ';
+            }
+        }
+        cout << '\n';
+    }
+    return 0;
+}
