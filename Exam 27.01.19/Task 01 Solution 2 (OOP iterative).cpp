eba74d902e5d4cd9c01959ef87c216f54e76d6d0
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-13 22:07:59 +0300

    Update Task 01 Solution 2 (OOP iterative).cpp

--- Exam 27.01.19/Task 01 Solution 2 (OOP iterative).cpp
+++ Exam 27.01.19/Task 01 Solution 2 (OOP iterative).cpp
@@ -1,78 +1,72 @@
+// github.com/andy489
+
+// https://www.hackerrank.com/contests/sda-exam-27-01-19-/challenges/challenge-1795
+
 #include <iostream>
 #include <stack>
+
 using namespace std;
 
 struct Node {
-	Node* left = nullptr, * right = nullptr;
-	int data;
-	Node(int data) :data(data) {}
+    Node *left = nullptr, *right = nullptr;
+    int data;
+
+    Node(int data) : data(data) {}
 };
 
 class BSTree {
-	Node* root = nullptr;
+    Node *root = nullptr;
 
 public:
 
-	void add(int data) {
-		Node* traversal = root;
-		Node* attach = nullptr;
-		while (traversal != nullptr) {
-			attach = traversal;
-			if (data > traversal->data) {
-				traversal = traversal->right;
-			}
-			else if (data < traversal->data) {
-				traversal = traversal->left;
-			}
-			else {
-				return;
-			}
-		}
-		if (attach == nullptr) {
-			root = new Node(data);
-		}
-		else if (data > attach->data) {
-			attach->right = new Node(data);
-		}
-		else if (data < attach->data) {
-			attach->left = new Node(data);
-		}			
-	}
-	void print() {
-		if (root == nullptr) {
-			return;
-		}
-		stack<Node*>s;
-		s.push(root);
-		while (!s.empty()) {
-			Node* cur = s.top();
-			s.pop();
-			cout << cur->data<<' ';
-			if (cur->right) {
-				s.push(cur->right);
-			}
-			if (cur->left) {
-				s.push(cur->left);
-			}
-		}
+    void add(int data) {
+        Node *traversal = root, *attach = nullptr;
+        while (traversal != nullptr) {
+            attach = traversal;
+            if (data > traversal->data)
+                traversal = traversal->right;
+            else if (data < traversal->data)
+                traversal = traversal->left;
+            else
+                return;
+        }
+        if (attach == nullptr)
+            root = new Node(data);
+        else if (data > attach->data)
+            attach->right = new Node(data);
+        else if (data < attach->data)
+            attach->left = new Node(data);
+    }
 
-	}
+    void print() {
+        if (root == nullptr)
+            return;
+        stack<Node *> s;
+        s.push(root);
+        while (!s.empty()) {
+            Node *cur = s.top();
+            s.pop();
+            cout << cur->data << ' ';
+            if (cur->right)
+                s.push(cur->right);
+            if (cur->left)
+                s.push(cur->left);
+        }
+    }
 };
 
 int main() {
-	BSTree t;
-	int q, data;
-	cin >> q;
-	string command;
-	while (q--) {
-		cin >> command;
-		if (command == "add") {
-			cin >> data;
-			t.add(data);
-		}
-		else if (command == "print") {
-			t.print();
-		}
-	}
-	return 0;
+    BSTree t;
+    int q, data;
+    cin >> q;
+    string cmd;
+    while (q--) {
+        cin >> cmd;
+        if (cmd == "add") {
+            cin >> data;
+            t.add(data);
+        } else if (cmd == "print")
+            t.print();
+    }
+    return 0;
 }
