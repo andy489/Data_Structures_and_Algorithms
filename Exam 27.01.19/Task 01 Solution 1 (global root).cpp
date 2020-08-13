b1ef6361cf2eb551e257e7ab80e6282f5ae72bbd
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-13 22:06:10 +0300

    Update Task 01 Solution 1 (global root).cpp

--- Exam 27.01.19/Task 01 Solution 1 (global root).cpp
+++ Exam 27.01.19/Task 01 Solution 1 (global root).cpp
@@ -1,48 +1,49 @@
-#include <bits/stdc++.h>
+// github.com/andy489
+
+// https://www.hackerrank.com/contests/sda-exam-27-01-19-/challenges/challenge-1795
+
+#include <iostream>
+
 using namespace std;
 
-struct Node{
-    Node *left=nullptr,*right=nullptr;
+struct Node {
+    Node *left = nullptr, *right = nullptr;
     int data;
-    Node(int data):data(data){}
+
+    Node(int data) : data(data) {}
 };
 
-Node* root; //it will be set to default value nullptr, because it's global variable
+Node *root; // global variables are set to their default value (in this case nullptr)
 
-Node* add(Node* root,int data){
-    if(root==nullptr){
+Node *add(Node *root, int data) {
+    if (root == nullptr)
         return new Node(data);
-    }
-    else if(data>root->data){
-        root->right=add(root->right,data);
-    }
-    else if(data<root->data){
-        root->left=add(root->left,data);
-    }
+    else if (data > root->data)
+        root->right = add(root->right, data);
+    else if (data < root->data)
+        root->left = add(root->left, data);
     return root;
 }
 
-void print(Node* root){
-    if(root!=nullptr){
-        cout<<root->data<<' ';
-        print(root->left);        
+void print(Node *root) {
+    if (root != nullptr) {
+        cout << root->data << ' ';
+        print(root->left);
         print(root->right);
     }
 }
 
 int main() {
-    int q,data;
-    cin>>q;
-    string command;
-    while(q--){
-        cin>>command;
-        if(command=="add"){
-            cin>>data;
-            root=add(root,data);
-        }
-        else if(command=="print"){
+    int q, data;
+    cin >> q;
+    string cmd;
+    while (q--) {
+        cin >> cmd;
+        if (cmd == "add") {
+            cin >> data;
+            root = add(root, data);
+        } else if (cmd == "print")
             print(root);
-        }
     }
     return 0;
 }
