bb2a378ba74ece0668db0656f6fbd5af7cedc624
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-29 11:13:47 +0300

    Create E. Bertown roads.cpp

---
+++ Graphs/E. Bertown roads.cpp
@@ -0,0 +1,70 @@
+// github.com/andy489
+ 
+// https://codeforces.com/contest/118/problem/E
+ 
+// Additional: https://codeforces.com/blog/entry/68138
+ 
+#include <iostream>
+#include <vector>
+#include <list>
+ 
+using namespace std;
+ 
+#define ios ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define pb push_back
+#define pii pair<int,int>
+ 
+vector<list<int>> adj;
+vector<bool> vis;
+vector<int> tin, low;
+ 
+int timer;
+ 
+vector<pii > de; // directed edges
+ 
+void dfs(int u, int par = -1) {
+    vis[u] = true;
+    tin[u] = low[u] = timer++;
+    for (const auto &child: adj[u]) {
+        if (child == par)
+            continue;
+        if (vis[child]) {
+            low[u] = min(low[u], tin[child]);
+            if (tin[child] < tin[u]) // back edge
+                de.emplace_back(u, child);
+        } else {
+            de.emplace_back(u, child);
+            dfs(child, u);
+            low[u] = min(low[u], low[child]);
+            if (low[child] > tin[u]) {
+                cout << 0;
+                exit(0);
+            }
+        }
+    }
+}
+ 
+int main() {
+    ios;
+    int n, m, u, v;
+    cin >> n >> m;
+ 
+    adj.resize(n + 1);
+    vis.resize(n + 1);
+    tin.resize(n + 1);
+    low.resize(n + 1);
+    de.reserve(m);
+    
+    while (m--) {
+        cin >> u >> v;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+ 
+    dfs(1);
+ 
+    for (const auto &e: de)
+        cout << e.first << ' ' << e.second << '\n';
+ 
+    return 0;
+}
