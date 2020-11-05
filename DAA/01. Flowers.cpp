44bb90ff04f88659e920adc037b3d7fb1b8d07f6
Andrey Stoev <akstoev@uni-sofia.bg>
2020-11-05 13:56:26 +0200

    Update 01. Flowers.cpp

--- DAA/01. Flowers.cpp
+++ DAA/01. Flowers.cpp
@@ -1,5 +1,5 @@
 // github.com/andy489
 
-https://judge.openfmi.net/timed_contest/get_problem_description?contest_id=177&problem_id=550
+// https://judge.openfmi.net/timed_contest/get_problem_description?contest_id=177&problem_id=550
 
 #include <cstdio>
