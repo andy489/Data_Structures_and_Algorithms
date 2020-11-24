6d571d891d3e631fde4b6112d853000f59cf59de
Andrey Stoev <akstoev@uni-sofia.bg>
2020-11-24 19:37:30 +0200

    Update 06.07. Elitism S2 (heap).cpp

--- FMI Data Structures & Algorithms/week 06/06.07. Elitism S2 (heap).cpp
+++ FMI Data Structures & Algorithms/week 06/06.07. Elitism S2 (heap).cpp
@@ -7,27 +7,4 @@
 
 using namespace std;
 
-int main() {
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-    priority_queue<int> maxHeap;
-    int N, a;
-
-    scanf("%d", &N);
-    while (N--) {
-        scanf("%d", &a);
-        maxHeap.push(a);
-        if (maxHeap.size() > minHeap.size() + 1)
-            minHeap.push(maxHeap.top()), maxHeap.pop();
-        else if (!minHeap.empty() && maxHeap.top() > minHeap.top()) {
-            maxHeap.push(minHeap.top()), minHeap.pop();
-            minHeap.push(maxHeap.top()), maxHeap.pop();
-        }
-        if (maxHeap.size() > minHeap.size())
-            printf("%d.0\n", maxHeap.top());
-        else {
-            a = maxHeap.top() + minHeap.top();
-            printf("%d.%d\n", a / 2, (a & 1) ? 5 : 0);
-        }
-    }
-    return 0;
-}
+//hidden because of current homework
