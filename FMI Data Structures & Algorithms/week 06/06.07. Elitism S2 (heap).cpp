963fc513ab542c9d80fb28fe79fa4ea40dc46c00
Andrey Stoev <akstoev@uni-sofia.bg>
2020-11-24 19:38:33 +0200

    Update 06.07. Elitism S2 (heap).cpp

--- FMI Data Structures & Algorithms/week 06/06.07. Elitism S2 (heap).cpp
+++ FMI Data Structures & Algorithms/week 06/06.07. Elitism S2 (heap).cpp
@@ -1,10 +1,1 @@
-// github.com/andy489
-
-// https://www.hackerrank.com/contests/practice-6-sda/challenges/elitism
-
-#include <stdio.h>
-#include <queue>
-
-using namespace std;
-
 //hidden because of current homework
