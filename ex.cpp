858edf0d16a015fffb6b8cd74bec52004c8b6aeb
Andrey Stoev <akstoev@uni-sofia.bg>
2020-12-21 11:08:56 +0200

    Delete ex.cpp

--- ex.cpp
+++
@@ -1,7 +0,0 @@
-void counting() {
-        int i(0);
-        for (i = 1; i <= states; ++i) ++c[len[i]];
-        for (i = 1; i <= states; ++i)c[i] += c[i - 1];
-        for (i = states; i; --i) a[c[len[i]]--] = i;
-        for (i = states; i; --i) cnt[par[a[i]]] += cnt[a[i]];
-    }
