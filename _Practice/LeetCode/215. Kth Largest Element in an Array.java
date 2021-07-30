2e7f12fe823f1e2033ea5a80c118dfa867a66a24
Andrey Stoev <akstoev@uni-sofia.bg>
2021-07-30 21:09:13 +0300

    Create 215. Kth Largest Element in an Array.java

---
+++ _Practice/LeetCode/215. Kth Largest Element in an Array.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+class Solution {
+    private static Random rand = new Random();
+
+    private static int partition(int[] nums, int l, int r) {
+        int pivot = l + rand.nextInt(r - l + 1);
+        nums[r] += nums[pivot] - (nums[pivot] = nums[r]);
+        int i = l, j = l;
+        for (; j <= r - 1; ++j) {
+            if (nums[j] >= nums[r]) {
+                nums[i] += nums[j] - (nums[j] = nums[i]);
+                i++;
+            }
+        }
+        nums[i] += nums[j] - (nums[j] = nums[i]);
+        return i;
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int pos = partition(nums, l, r);
+            if (pos == k - 1) {
+                return nums[pos];
+            }
+            if (pos < k - 1) {
+                l = pos + 1;
+            } else {
+                r = pos - 1;
+            }
+        }
+        return Integer.MIN_VALUE;
+    }
+}
