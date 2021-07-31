855f8a6f8c97a2f3c1629fc4b5da4864a5357925
Andrey Stoev <akstoev@uni-sofia.bg>
2021-07-31 10:23:11 +0300

    Create 1. Two Sum.java

---
+++ _Practice/LeetCode/1. Two Sum.java
@@ -0,0 +1,18 @@
+// github.com/andy489
+// https://leetcode.com/problems/two-sum/
+// Time: O(n) in expectation
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> lookUp = new HashMap<>();
+        int SIZE = nums.length;
+        for (int i = 0; i < SIZE; ++i) {
+            if (lookUp.containsKey(target - nums[i])) {
+                return new int[]{i, lookUp.get(target - nums[i])};
+            } else {
+                lookUp.put(nums[i], i);
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
