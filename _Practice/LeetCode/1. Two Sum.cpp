4aee9cc41dca3d8dd78e29e6ed00c59f45d4fd74
Andrey Stoev <akstoev@uni-sofia.bg>
2021-07-31 10:09:44 +0300

    Create 1. Two Sum.cpp

---
+++ _Practice/LeetCode/1. Two Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        vector<int> res(2);
+        unordered_map<int, int> lookUp;
+        int SIZE = nums.size();
+        for (int i = 0; i < SIZE; ++i) {
+            int complement = target - nums[i];
+            if (lookUp.count(complement)) {
+                res[0] = i, res[1] = lookUp[complement];
+                break;
+            } else {
+                lookUp.insert({nums[i], i});
+            }
+        }
+        return res;
+    }
+};
