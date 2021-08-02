df8e8dcea7ca3c55bf7fb3e057c940fdf38466aa
Andrey Stoev <akstoev@uni-sofia.bg>
2021-08-02 16:43:09 +0300

    Create 005. Longest Palindromic Substring.cpp

---
+++ _Practice/LeetCode/005. Longest Palindromic Substring.cpp
@@ -0,0 +1,33 @@
+// Brute force
+// https://leetcode.com/problems/longest-palindromic-substring/
+// Time: O(n^2)
+
+class Solution {
+private:
+    pair<int, int> expandPalindrome(string &s, int l, int r) {
+        while (0 <= l && r < s.size() && s[l] == s[r]) { l--, r++; }
+        return {l + 1, r};
+    }
+
+    int substrLength(pair<int, int> substr) {
+        return substr.second - substr.first + 1;
+    }
+
+public:
+    string longestPalindrome(string s) {
+        int SIZE = s.size();
+        pair<int, int> lps = {0, 0};
+        for (int i = 0; i < SIZE; ++i) {
+            pair<int, int> odd = expandPalindrome(s, i, i);
+            pair<int, int> even = expandPalindrome(s, i, i + 1);
+            if (substrLength(lps) < substrLength(odd)) {
+                lps = odd;
+            }
+            if (substrLength(lps) < substrLength(even)) {
+                lps = even;
+            }
+        }
+        string res(s.begin() + lps.first, s.begin() + lps.second);
+        return res;
+    }
+};
