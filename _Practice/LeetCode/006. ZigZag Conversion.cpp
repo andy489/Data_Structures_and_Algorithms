dd7a0cf059ea7b2480f2c9c2f44650a42232be51
Andrey Stoev <akstoev@uni-sofia.bg>
2021-08-03 12:34:54 +0300

    Create 006. ZigZag Conversion.cpp

---
+++ _Practice/LeetCode/006. ZigZag Conversion.cpp
@@ -0,0 +1,23 @@
+// the sequence of line numbers in which a letter is placed: 0, 1, 2, ..., numRows - 1, numRows - 2, ..., 0, 1, 2, ..., numRows - 1, numRows - 2, ...
+// https://leetcode.com/problems/zigzag-conversion/
+// Time: O(n)
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        vector <string> rows(numRows);
+        int i = 0, step = 0;
+        string res;
+        for (char c : s) {
+            rows[i].push_back(c);
+            if (i == 0) step = 1;
+            if (i == numRows - 1) step = -1;
+            i += step;
+        }
+        for (int i = 0; i < numRows; ++i) {
+            res += rows[i];
+        }
+        return res;
+    }
+};
