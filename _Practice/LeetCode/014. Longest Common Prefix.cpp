58266135756518a749c914ad51e593ba3537bbde
Andrey Stoev <akstoev@uni-sofia.bg>
2021-08-12 12:00:22 +0300

    Update 014. Longest Common Prefix.cpp

--- _Practice/LeetCode/014. Longest Common Prefix.cpp
+++ _Practice/LeetCode/014. Longest Common Prefix.cpp
@@ -3,11 +3,11 @@
 
 class Solution {
 public:
-    string longestCommonPrefix(vector <string> &strs) {
+    string longestCommonPrefix(vector<string>& strs) {
         sort(strs.begin(), strs.end());
         string l = strs[0], r = strs.back();
-        int i = 0, SIZE = min(l.size(), r.size());
-        for (; i < SIZE; ++i) if (l[i] != r[i]) break;
+        int i = 0, SIZE = l.size();
+        for(; i < SIZE; ++i) if(l[i] != r[i]) break;
         return l.substr(0, i);
     }
 };
