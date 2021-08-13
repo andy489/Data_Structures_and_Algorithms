ec9912b5b8c9f0172f83e2e1dc9a52e6fdba8cc7
Andrey Stoev <akstoev@uni-sofia.bg>
2021-08-13 18:29:16 +0300

    Update and rename 018. Letter Combinations of a Phone Number (bfs).cpp to 018. Letter Combinations of a Phone Number (iterative with vectors).cpp

---
+++ _Practice/LeetCode/018. Letter Combinations of a Phone Number (iterative with vectors).cpp
@@ -0,0 +1,28 @@
+// backtrack, dfs
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+// Time: O(4^n), where n is the number of digits
+
+class Solution {
+    const vector <string> pad = {
+            "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+    };
+    int n;
+    vector <string> res;
+public:
+    vector <string> letterCombinations(string digits) {
+        n = digits.size();
+        if (!n) return {};
+        res.push_back("");
+        for (char d:digits) {
+            vector <string> tmp;
+            for (char candidate: pad[d - '2']) {
+                for (string s: res) {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            res.swap(tmp);
+        }
+        return res;
+    }
+};
