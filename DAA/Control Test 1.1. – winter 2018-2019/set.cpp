89cd55a3204fbc561134486a380071529105db72
Andrey Stoev <akstoev@uni-sofia.bg>
2021-12-03 12:17:25 +0200

    Create set.cpp

---
+++ DAA/Control Test 1.1. – winter 2018-2019/set.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    ll k;
+    scanf("%d%lld", &n, &k);
+
+    vector<ll> v(n);
+    unordered_set<ll> us;
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%lld", &v[i]);
+    }
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    for (int i = 0; i < n; ++i) {
+        if(!us.count(v[i]*k)){
+            us.insert(v[i]);
+        }
+    }
+
+    printf("%lu", us.size());
+}
