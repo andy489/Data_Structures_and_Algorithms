e3ac18cfa33b368c6af4f52eb859f3c4ff064c36
Andrey Stoev <akstoev@uni-sofia.bg>
2022-02-13 18:16:04 +0200

    Update 10.01. Minimal forest (Prim).cpp

--- FMI Data Structures & Algorithms/week 10/10.01. Minimal forest (Prim).cpp
+++ FMI Data Structures & Algorithms/week 10/10.01. Minimal forest (Prim).cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-static const int INF = 2 << 10;
+static const int INF = 1 << 10;
 
 struct Edge {
     int to = -1;
@@ -25,12 +25,12 @@ int n;
 
 vector<vector<Edge>> adj;
 vector<bool> visited;
+vector<Edge> mst;
 
 int prim(int start) {
     int totalCost = 0;
 
-    vector<Edge> minEdge(n);
-    minEdge[start].cost = 0;
+    mst[start].cost = 0;
 
     set<Edge> s;
     s.insert({start, 0});
@@ -48,9 +48,9 @@ int prim(int start) {
 
 
         for (const Edge e : adj[curr.to]) {
-            if (!visited[e.to] && e.cost < minEdge[e.to].cost) {
-                s.erase({e.to, minEdge[e.to].cost});
-                minEdge[e.to] = {curr.to, e.cost};
+            if (!visited[e.to] && e.cost < mst[e.to].cost) {
+                s.erase({e.to, mst[e.to].cost});
+                mst[e.to] = {curr.to, e.cost};
                 s.insert({e.to, e.cost});
             }
         }
@@ -65,6 +65,7 @@ void init() {
 
     adj.resize(n);
     visited.resize(n);
+    mst.resize(n);
 
     int from, to, cost;
     while (m--) {
@@ -87,6 +88,35 @@ int solve() {
     return minimalForestCost;
 }
 
-int main() {
-    return init(), printf("%d", solve()), 0;
+void printMST() {
+    for (int i = 0; i < n; ++i) {
+        Edge curr = mst[i];
+
+        if (curr.to != -1) {
+            printf("\n<%d, %d>, <%d>", i, curr.to, curr.cost);
+        }
+    }
 }
+
+int main() {
+    init();
+    printf("%d", solve());
+    // printMST();
+
+    return 0;
+}
+
+/*
+9 11
+0 3 2
+4 5 2
+3 7 5
+1 3 5
+5 2 4
+5 8 2
+1 7 2
+0 7 4
+4 8 3
+0 1 3
+2 8 3
+*/
