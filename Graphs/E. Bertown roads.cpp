922eb89c3211f022fbee40b3f85d0001d0508a37
Andrey Stoev <akstoev@uni-sofia.bg>
2022-02-14 18:36:24 +0200

    Update E. Bertown roads.cpp

--- Graphs/E. Bertown roads.cpp
+++ Graphs/E. Bertown roads.cpp
@@ -1,4 +1,4 @@
-// https://www.spoj.com/problems/EC_P/
+// https://codeforces.com/contest/118/problem/E
 // https://codeforces.com/blog/entry/68138
 
 #include <cstdio>
@@ -10,78 +10,71 @@ using namespace std;
 
 int timer;
 
+vector<list<int>> adj;
 vector<bool> vis; // visited
 vector<int> tin; // time of entry into node
 vector<int> low; // low[v] = min{tin[v], tin[par], low[to]},
-// for all par for which (v, par) is a back edge
-// for all to for which (v, to) is a tree edge
-vector<list<int>> adj;
+// for all par for which (v, par) is a back edge, for all to for which (v, to) is a tree edge
 
-// tin = time of entry into node
-// low -> none of the vertices child and its descendants in the DFS traversal tree
-// has a back-edge to vertex v or any of its ancestors (v, child)
-
+set<pair<int, int>> dfs_spanning_edges;
+set<pair<int, int>> back_edges;
 set<pair<int, int>> bridges;
 
 void tarjan(int u = 1, int par = -1) {
     vis[u] = true;
     tin[u] = low[u] = timer++;
 
-    for (const auto &child : adj[u]) {
+    if (par != -1) {
+        dfs_spanning_edges.insert({par, u});
+    }
+
+    for (const int &child : adj[u]) {
         if (child == par) {
             continue;
         }
 
         if (vis[child]) {
             low[u] = min(low[u], tin[child]);
+            if (back_edges.find({child, u}) == back_edges.end()) {
+                back_edges.insert({u, child});
+            }
         } else {
             tarjan(child, u);
             low[u] = min(low[u], low[child]);
 
             if (low[child] > tin[u]) {
-                bridges.insert({min(child, u), max(child, u)});
+                bridges.insert({u, child});
             }
         }
     }
 }
 
 int main() {
-    int t;
-    scanf("%d", &t);
-
     int n, m;
-    for (int i = 1; i <= t; ++i) {
-        scanf("%d %d", &n, &m);
+    scanf("%d %d", &n, &m);
 
-        adj.assign(n + 1, list<int>());
-        vis.assign(n + 1, false);
-        tin.assign(n + 1, 0);
-        low.assign(n + 1, 0);
+    adj.resize(n + 1);
+    vis.resize(n + 1);
+    tin.resize(n + 1);
+    low.resize(n + 1);
 
-        int a, b;
-        while (m--) {
-            scanf("%d %d", &a, &b);
-            adj[a].push_back(b);
-            adj[b].push_back(a);
-        }
-
-        printf("Caso #%d\n", i);
-
-        tarjan();
-
-        if (bridges.empty()) {
-            printf("Sin bloqueos\n");
-        } else {
-            printf("%lu\n", bridges.size());
-
-            for (const auto &br: bridges) {
-                printf("%d %d\n", br.first, br.second);
-            }
-        }
-
-        bridges.clear();
-        timer = 0;
+    int a, b;
+    while (m--) {
+        scanf("%d %d", &a, &b);
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
 
-    return 0;
+    tarjan();
+
+    if (bridges.empty()) {
+        for (const auto &sp:dfs_spanning_edges) {
+            printf("%d %d\n", sp.first, sp.second);
+        }
+        for (const auto &be:back_edges) {
+            printf("%d %d\n", be.first, be.second);
+        }
+    } else {
+        printf("0");
+    }
 }
