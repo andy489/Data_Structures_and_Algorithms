a21fb2943237feb2fc0b35760cc390b504f364ec
Andrey Stoev <akstoev@uni-sofia.bg>
2022-02-15 19:06:14 +0200

    Update 09.02. Christmas markets S2 (shortest cycle).cpp

--- FMI Data Structures & Algorithms/week 09/09.02. Christmas markets S2 (shortest cycle).cpp
+++ FMI Data Structures & Algorithms/week 09/09.02. Christmas markets S2 (shortest cycle).cpp
@@ -1,106 +1,88 @@
-// github.com/andy489
-
 // https://www.hackerrank.com/contests/practice-8-sda/challenges/challenge-2303
 
-#include <iostream>
-#include <list>
+#include <cstdio>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
-struct Node {
-    string name;
-    string parent;
-    bool visited;
-    list<Node *> out;
+int n;
+char start[10];
+unordered_map<string, vector<string>> adj;
+unordered_set<string> visited;
+unordered_map<string, string> parent;
+stack<string> shortestRouteReversed;
 
-    Node(string name = "", string parent = "", bool visited = false) :
-            name(name), parent(parent), visited(visited) {};
+bool bfs(const string &start) {
+    visited.insert(start);
 
-    ~Node() {
-        out.clear();
-    }
-};
-
-bool bfs(const string &start, unordered_map<string, Node *> &um) {
-    um[start]->visited = true;
     queue<string> Q;
     Q.push(start);
 
     while (!Q.empty()) {
-        string curr = Q.front();
+        string currentCity = Q.front();
         Q.pop();
-        for (const auto &x : um[curr]->out) {
-            if (!x->visited) {
-                x->visited = true;
-                Q.push(x->name);
-                x->parent = curr;
-            } else {
-                if (x->name == start) {
-                    x->parent = curr;
-                    return true;
-                }
+
+        for (const auto &city : adj[currentCity]) {
+            if (!visited.count(city)) {
+                visited.insert(city);
+                Q.push(city);
+                parent[city] = currentCity;
+                continue;
+            }
+
+            if (city == start) {
+                parent[city] = currentCity;
+                return true;
             }
         }
     }
     return false;
 }
 
-void freeMap(unordered_map<string, Node *> &um) {
-    auto it = um.begin();
-    for (;it != um.end(); ++it)
-        delete it->second;
+void init(int m) {
+    char city1[10], city2[10];
+    
+    while (m--) {
+        scanf("%s %s", city1, city2);
+        adj[city1].push_back(city2);
+    }
+
+    scanf("%s", start);
+}
+
+void extract_route() {
+    shortestRouteReversed.push(start);
+    string traversal = start;
+    
+    while (parent[traversal] != start) {
+        traversal = parent[traversal];
+        shortestRouteReversed.push(traversal);
+    }
+    shortestRouteReversed.push(start);
+}
+
+void display_route() {
+    while (!shortestRouteReversed.empty()) {
+        printf("%s ", shortestRouteReversed.top().c_str());
+        shortestRouteReversed.pop();
+    }
 }
 
 int main() {
-    int n, m;
-    cin >> n >> m;
+    int m;
+    scanf("%d %d", &n, &m);
 
-    string city1, city2;
+    init(m);
 
-    unordered_map<string, Node *> um;
-
-    while (m--) {
-        cin >> city1 >> city2;
-
-        Node *CITY1 = nullptr,
-                *CITY2 = nullptr;
-
-        if (!um.count(city1)) {
-            CITY1 = new Node(city1);
-            um[city1] = CITY1;
-        }
-
-        if (!um.count(city2)) {
-            CITY2 = new Node(city2);
-            um[city2] = CITY2;
-        } else {
-            CITY2 = um[city2];
-        }
-        um[city1]->out.push_back(CITY2);
-    }
-
-    string start;
-    cin >> start;
-
-    list<string> possibleCycle; // this is even the shortest cycle
-
-    if (bfs(start, um)) {
-        possibleCycle.push_front(start);
-        string traverse = um[start]->parent;
-        while (traverse != start) {
-            possibleCycle.push_front(um[traverse]->name);
-            traverse = um[traverse]->parent;
-        }
+    if (bfs(start)) {
+        extract_route();
     } else {
-        cout << "-1";
-        return freeMap(um), 0;
+        return printf("-1"), 0;
     }
-    possibleCycle.push_front(start);
 
-    for (const auto &x : possibleCycle)
-        cout << x << ' ';
-    return freeMap(um), 0;
+    return display_route(), 0;
 }
