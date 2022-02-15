fcc8e4e26321de06a948a716310167bdaf5de75a
Andrey Stoev <akstoev@uni-sofia.bg>
2022-02-15 22:26:12 +0200

    Update Articulation Points and Bridges (1st sol.).cpp

--- Graphs/Articulation Points and Bridges (1st sol.).cpp
+++ Graphs/Articulation Points and Bridges (1st sol.).cpp
@@ -1,76 +1,87 @@
-// github.com/andy489
-
 // https://www.hackerearth.com/practice/algorithms/graphs/articulation-points-and-bridges/tutorial/
 
-#include <iostream>
+#include <cstdio>
 #include <vector>
 #include <list>
 #include <set>
 
 using namespace std;
 
-#define ios ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
-#define pb push_back
-#define f first
-#define s second
-
-typedef pair<int, int> pii;
-
-int timer;
-
+int n;
 vector<bool> vis;
 vector<int> tin, low;
 vector<list<int>> adj;
 
-set<int> artPts;
-set<pii> bridges;
+set<int> articulation_points;
+set<pair<int, int>> bridges;
+
+int timer;
 
 void tarjan(int u = 0, int p = -1) {
     vis[u] = true;
     tin[u] = low[u] = timer++;
+
     int children = 0;
-    for (const int &child: adj[u]) {
-        if (child == p) continue;
-        if (vis[child]) low[u] = min(low[u], tin[child]);
-        else {
+    for (int child: adj[u]) {
+        if (child == p) {
+            continue;
+        }
+
+        if (vis[child]) {
+            low[u] = min(low[u], tin[child]);
+        } else {
             tarjan(child, u);
             low[u] = min(low[u], low[child]);
-            if (low[child] > tin[u])
+
+            if (low[child] > tin[u]) {
                 bridges.insert({min(u, child), max(u, child)});
-            if (low[child] >= tin[u] && p != -1)
-                artPts.insert(u);
+            }
+
+            if (low[child] >= tin[u] && p != -1) {
+                articulation_points.insert(u);
+            }
+
             ++children;
         }
     }
-    if (p == -1 && children > 1)
-        artPts.insert(u);
+    
+    if (p == -1 && children > 1) {
+        articulation_points.insert(u);
+    }
 }
 
-int main() {
-    ios;
-    int n, m, u, v;
-    cin >> n >> m;
+void init() {
+    int m;
+    scanf("%d %d", &n, &m);
 
     adj.resize(n);
     vis.resize(n);
     tin.resize(n);
     low.resize(n);
 
+    int u, v;
     while (m--) {
-        cin >> u >> v;
-        adj[u].pb(v);
-        adj[v].pb(u);
+        scanf("%d %d", &u, &v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
+}
 
+int main() {
+    init();
     tarjan();
 
-    cout << artPts.size() << '\n';
-    for (const auto &pt: artPts)
-        cout << pt << ' ';
-    cout << '\n';
-    cout << bridges.size() << '\n';
-    for (const pii& e: bridges)
-        cout << e.f << ' ' << e.s << '\n';
+    printf("%lu\n", articulation_points.size());
+
+    for (const auto &art_pt: articulation_points) {
+        printf("%d ", art_pt);
+    }
+
+    printf("\n%lu\n", bridges.size());
+
+    for (const pair<int, int> &bridge: bridges) {
+        printf("%d %d\n", bridge.first, bridge.second);
+    }
 
     return 0;
 }
