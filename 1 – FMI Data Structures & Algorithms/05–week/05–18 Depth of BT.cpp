2e0273f68310f95b035931cc425fc13af84b870a
Andrey Stoev <akstoev@uni-sofia.bg>
2022-03-05 02:25:38 +0200

    Update 05–18 Depth of BT.cpp

--- 1 – FMI Data Structures & Algorithms/05–week/05–18 Depth of BT.cpp
+++ 1 – FMI Data Structures & Algorithms/05–week/05–18 Depth of BT.cpp
@@ -1,3 +1,19 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/*
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+*/
+
+
 #include <iostream>
 
 using namespace std;
