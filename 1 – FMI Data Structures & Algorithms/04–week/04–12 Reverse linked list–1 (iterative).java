ed586351113d2b9621c3af79ccf296736d24cda6
andy489 <andrey.s@abv.bg>
2022-03-28 23:49:13 +0300

    Reverse Linked List Iterative Java

--- 1 – FMI Data Structures & Algorithms/04–week/04–12 Reverse linked list–1 (iterative).java
+++ 1 – FMI Data Structures & Algorithms/04–week/04–12 Reverse linked list–1 (iterative).java
@@ -1,1 +1,16 @@
 // https://www.hackerrank.com/challenges/reverse-a-linked-list/problem
+
+public static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+    SinglyLinkedListNode traverse = head;
+    SinglyLinkedListNode prev = null;
+
+    while (traverse != null) {
+        SinglyLinkedListNode next = traverse.next;
+
+        traverse.next = prev;
+        prev = traverse;
+        traverse = next;
+    }
+
+    return prev;
+}
