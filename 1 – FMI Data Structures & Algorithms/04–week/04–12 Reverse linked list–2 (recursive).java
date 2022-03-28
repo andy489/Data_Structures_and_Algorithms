caf621fe83473071243c9d4f220ee1629d181693
andy489 <andrey.s@abv.bg>
2022-03-29 00:12:20 +0300

    Reverse Linked List Recursive Java

--- 1 – FMI Data Structures & Algorithms/04–week/04–12 Reverse linked list–2 (recursive).java
+++ 1 – FMI Data Structures & Algorithms/04–week/04–12 Reverse linked list–2 (recursive).java
@@ -1,1 +1,14 @@
 // https://www.hackerrank.com/challenges/reverse-a-linked-list/problem
+
+public static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+    if (head == null || head.next == null) {
+        return head;
+    }
+
+    SinglyLinkedListNode remaining = reverse(head.next);
+
+    head.next.next = head;
+    head.next = null;
+
+    return remaining;
+}
